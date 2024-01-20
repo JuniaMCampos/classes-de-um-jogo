7a5de4365d4e5724d542f5804991702cc5d9ebf1
JuniaMCampos <juniacamposjc@gmail.com>
2024-01-20 18:53:07 -0300

    soluçao desenvolvida

---
+++ index.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'ataque indefinido';
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi = new Heroi('Herói Mago', 25, 'mago');
+heroi.atacar();
+
+
